5d8e6c97ef707f2d67061fae1e968fe2676bfb8d
Paulina Osikoya <paulinaosikoya@gmail.com>
2024-04-17 18:44:34 +0100

    add classification report of model accuracy

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # ML-Cancer-Prediction-Logistic-Regression
 Python based application that predict benign and malignant cancer cells 
-update note 
--- model/main.py
+++ model/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.metrics import accuracy_score, classification_report
 
 
 def create_model(data):
@@ -21,6 +21,11 @@ def create_model(data):
     #train
     model = LogisticRegression()
     model.fit(X_train, y_train)
+
+    #test model
+    y_pred = model.predict(X_test)
+    print('Accuracy of model: ', accuracy_score(y_test, y_pred))
+    print("Classification Report: \n", classification_report(y_test, y_pred))
 
     return model, scaler
 
