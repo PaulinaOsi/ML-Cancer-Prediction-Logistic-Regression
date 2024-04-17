44558d51b53ca4ab4df4b87d5e16048fdc92b6a1
Paulina Osikoya <paulinaosikoya@gmail.com>
2024-04-17 20:21:43 +0100

    adding pkl files and exporting as binaries for app dev

--- model/main.py
+++ model/main.py
@@ -3,26 +3,28 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+import pickle5 as pickle
+import os
 
 
 def create_model(data):
-    X = data.drop(['diagnosis'], axis =1)
+    X = data.drop(['diagnosis'], axis=1)
     y = data['diagnosis']
 
-    #scale data 
+    # scale data
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
 
-    #split data
+    # split data
     X_train, X_test, y_train, y_test = train_test_split(
-        X,y, test_size=0.2, random_state=42
+        X, y, test_size=0.2, random_state=42
     )
 
-    #train
+    # train
     model = LogisticRegression()
     model.fit(X_train, y_train)
 
-    #test model
+    # test model
     y_pred = model.predict(X_test)
     print('Accuracy of model: ', accuracy_score(y_test, y_pred))
     print("Classification Report: \n", classification_report(y_test, y_pred))
@@ -43,9 +45,16 @@ def get_clean_data():
 
 def main():
     data = get_clean_data()
-    #print(data.head())
+    # print(data.head())
 
     model, scaler = create_model(data)
+
+    with open('./model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+    with open('./scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
+
+# export model to binary file
 
 
 if __name__ == '__main__':
